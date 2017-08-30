3d7acd00faab352e12b6664707844965b27cf3b1
ybiquitous <ybiquitous@gmail.com>
2017-08-31 03:08:00 +0900

    test: migrate test framework from ava to mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
+
   extends: ['ybiquitous'],
+
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
 }
--- src/init.test.js
+++ src/init.test.js
@@ -1,51 +1,54 @@
-import test from 'ava'
 import path from 'path'
 import os from 'os'
 import fs from 'fs-extra'
+import assert from 'assert'
 import init from './init'
 
-let tmpdir
-let packageJson
+suite('init', () => {
+  let tmpdir
+  let packageJson
 
-test.before('setup work directory', async () => {
-  tmpdir = path.join(os.tmpdir(), `cli-${Date.now()}`)
-  await fs.mkdirs(tmpdir)
-})
-
-test.before('setup package.json', async () => {
-  packageJson = path.join(tmpdir, 'package.json')
-  await fs.writeJson(packageJson, {
-    scripts: { test: 'abc' },
-    'lint-staged': { '*.css': 'xyz' },
+  setup('work directory', async () => {
+    tmpdir = path.join(os.tmpdir(), `cli-${Date.now()}`)
+    await fs.mkdirs(tmpdir)
   })
-})
 
-test.after.always(async () => {
-  await fs.remove(tmpdir)
-})
-
-test('init', async (t) => {
-  await init(tmpdir)
-
-  const pkg = await fs.readJson(packageJson)
-  t.deepEqual(pkg.scripts, {
-    commitmsg: 'commitlint -e',
-    'lint:js': 'eslint --max-warnings=0 .',
-    'lint:js:fix': 'eslint --max-warnings=0 . --fix',
-    'lint:md': 'markdownlint *.md',
-    lint: 'npm-run-all --parallel lint:js lint:md',
-    precommit: 'lint-staged',
-    release: 'standard-version',
-    'release:dry-run': 'standard-version --dry-run',
-    test: 'abc',
-    'test:watch': 'abc --watch',
+  setup('package.json', async () => {
+    packageJson = path.join(tmpdir, 'package.json')
+    await fs.writeJson(packageJson, {
+      scripts: { test: 'abc' },
+      'lint-staged': { '*.css': 'xyz' },
+    })
   })
-  t.deepEqual(pkg['lint-staged'], {
-    '*.{js,jsx}': [
-      'eslint --fix --max-warnings=0 --no-ignore',
-      'git add',
-    ],
-    '*.md': 'markdownlint',
-    '*.css': 'xyz',
+
+  teardown(async () => {
+    await fs.remove(tmpdir)
+  })
+
+  test('success', async () => {
+    await init(tmpdir)
+    const pkg = await fs.readJson(packageJson)
+
+    assert.deepStrictEqual(pkg.scripts, {
+      commitmsg: 'commitlint -e',
+      'lint:js': 'eslint --max-warnings=0 .',
+      'lint:js:fix': 'eslint --max-warnings=0 . --fix',
+      'lint:md': 'markdownlint *.md',
+      lint: 'npm-run-all --parallel lint:js lint:md',
+      precommit: 'lint-staged',
+      release: 'standard-version',
+      'release:dry-run': 'standard-version --dry-run',
+      test: 'abc',
+      'test:watch': 'abc --watch',
+    })
+
+    assert.deepStrictEqual(pkg['lint-staged'], {
+      '*.{js,jsx}': [
+        'eslint --fix --max-warnings=0 --no-ignore',
+        'git add',
+      ],
+      '*.md': 'markdownlint',
+      '*.css': 'xyz',
+    })
   })
 })
