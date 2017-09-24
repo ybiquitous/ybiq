95fd61f011d74a21c85e98d7d27c91695440a763
ybiquitous <ybiquitous@gmail.com>
2017-09-25 03:29:13 +0900

    chore(commitlint): restrict possible scopes (#11)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
+
+  rules: {
+    'scope-enum': [2, 'always', [
+      '',
+      'package',
+      'readme',
+      'eslint',
+      'commitlint',
+      'coverage',
+      'release',
+    ]],
+  },
 }
