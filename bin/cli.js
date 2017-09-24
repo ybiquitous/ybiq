e0e420b73e016ae59b541169a1da14d2e2720be2
ybiquitous <ybiquitous@gmail.com>
2017-09-25 04:07:12 +0900

    perf: prefer not babel code in Node 8+

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-require('../lib/cli')()
+const semver = require('semver')
+
+if (semver.satisfies(process.version, '>=8')) {
+  require('../src/cli')() // eslint-disable-line global-require
+} else {
+  require('../lib/cli')() // eslint-disable-line global-require
+}
