9c22a802dbea582d14e9fa77d35b541382eb7b33
ybiquitous <ybiquitous@gmail.com>
2017-09-28 01:34:09 +0900

    feat(api): support programmatic API (#21)

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
+const dir = require('../src/select-dir')
 
-const semver = require('semver')
-
-if (semver.satisfies(process.version, '>=8')) {
-  require('../src/cli')() // eslint-disable-line global-require
-} else {
-  require('../lib/cli')() // eslint-disable-line global-require
-}
+require(`../${dir}/cli`)() // eslint-disable-line import/no-dynamic-require
---
+++ test/main.test.js
@@ -0,0 +1,10 @@
+import assert from 'assert'
+import main from '..'
+
+suite('main', () => {
+  test('init', async () => {
+    const init = main.init
+    assert(typeof init === 'function')
+    assert(init.name === 'init')
+  })
+})
