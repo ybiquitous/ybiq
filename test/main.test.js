be5706493f6e92cbda380ddafee9e4f522311c3b
ybiquitous <ybiquitous@gmail.com>
2017-10-10 14:56:46 +0900

    chore(package): update eslint-config-ybiquitous to 3.0.0 (#24)
    - Fix new ESLint rule [prefer-destructuring]

--- test/main.test.js
+++ test/main.test.js
@@ -3,7 +3,7 @@ import main from '..'
 
 suite('main', () => {
   test('init', async () => {
-    const init = main.init
+    const { init } = main
     assert(typeof init === 'function')
     assert(init.name === 'init')
   })
