4394d0b8df5a0b0226a3f87b3e276f4ee22e855f
ybiquitous <ybiquitous@gmail.com>
2017-10-10 14:29:17 +0900

    Revert "ci: add debug code"
    This reverts commit c0263c0e6bfad905f969e1b730d1ab114922e17e.

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 const dir = require('../src/select-dir')
 
-// for debug in Travis
-process.stdout.write(`dir: ${dir}\n`)
-
 require(`../${dir}/cli`)() // eslint-disable-line import/no-dynamic-require
