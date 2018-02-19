9061177344b2fad6a1fd3bdd3964deb7ef2af893
Masafumi Koba <ybiquitous@gmail.com>
2018-02-19 18:50:05 +0900

    build: rename .babelrc to .babelrc.js (JSON -> JS format) (#124)

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: '6' } }]],
+
+  plugins: ['@babel/plugin-proposal-object-rest-spread'],
+
+  env: {
+    test: {
+      plugins: ['istanbul'],
+    },
+  },
+}
