375a6effc4479c622f0c7b6bde0ad55605fe297a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-14 00:37:10 +0900

    feat(deps): bump prettier from 1.19.1 to 2.0.4 (#593)
    See the [Prettier v2 release note](https://prettier.io/blog/2020/03/21/2.0.0.html).
    
    BREAKING CHANGE: Prettier v2; change defaults and use `.` instead of `**.{ext1,ext2}`.

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,8 @@
 const test = require("tape");
 const main = require("..");
 
-test("main", t => {
-  t.test("init", t => {
+test("main", (t) => {
+  t.test("init", (t) => {
     const { init } = main;
     t.is(typeof init, "function");
     t.is(init.name, "init");
