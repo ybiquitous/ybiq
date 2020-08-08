dc49f1350ed4eccf1effd679c5ac0e28ed8439cb
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-08 13:46:01 +0900

    test: tape -> jest
    https://jestjs.io/

--- test/version.test.js
+++ test/version.test.js
@@ -1,14 +1,10 @@
-const test = require("tape");
 const pkg = require("../package.json");
 const exec = require("./helpers/exec");
 
-test("version", (t) => {
-  ["--version", "-v"].forEach((option) => {
-    t.test(`with "${option}" option`, async (t) => {
-      const { stdout, stderr } = await exec(option);
-      t.is(stdout.trim(), pkg.version);
-      t.is(stderr, "");
-      t.end();
-    });
+["--version", "-v"].forEach((option) => {
+  test(`with "${option}" option`, async () => {
+    const { stdout, stderr } = await exec(option);
+    expect(stdout).toEqual(`${pkg.version}\n`);
+    expect(stderr).toEqual("");
   });
 });
