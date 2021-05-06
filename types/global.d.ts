01251de5a2baf3d6b099e02cdf87e1679eb3e5e9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-05-07 02:23:16 +0900

    chore: fix fs/promises on Node.js 12

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,4 @@
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34960#issuecomment-576906058
 declare let URL: typeof globalThis extends { URL: infer URLCtor }
   ? URLCtor
-  : typeof import("url").URL;
+  : typeof import("url").URL; // eslint-disable-line @typescript-eslint/consistent-type-imports
