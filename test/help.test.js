cddb254dba269289272a52015cd76c9201d96db9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-05-06 22:44:27 +0900

    feat: remove `bin/` directory
    This change aims to make the code management easier.

--- test/help.test.js
+++ test/help.test.js
@@ -1,17 +1,18 @@
+const pkg = require("../package.json");
 const exec = require("./helpers/exec");
 
 [[], ["unknown"], ["unknown", "xyz"]].forEach((args) => {
   test(`with arguments [${args.join(", ")}]`, async () => {
-    await expect(exec("bin/cli.js", ...args)).rejects.toThrowErrorMatchingSnapshot();
-    await expect(exec("bin/cli.js", ...args)).rejects.toHaveProperty("code", 1);
-    await expect(exec("bin/cli.js", ...args)).rejects.toHaveProperty("stdout", "");
-    await expect(exec("bin/cli.js", ...args)).rejects.toMatchSnapshot("stderr");
+    await expect(exec(pkg.bin, ...args)).rejects.toThrowErrorMatchingSnapshot();
+    await expect(exec(pkg.bin, ...args)).rejects.toHaveProperty("code", 1);
+    await expect(exec(pkg.bin, ...args)).rejects.toHaveProperty("stdout", "");
+    await expect(exec(pkg.bin, ...args)).rejects.toMatchSnapshot("stderr");
   });
 });
 
 ["--help", "-h"].forEach((option) => {
   test(`with "${option}" option`, async () => {
-    const { stdout, stderr } = await exec("bin/cli.js", option);
+    const { stdout, stderr } = await exec(pkg.bin, option);
     expect(stdout).toMatchSnapshot();
     expect(stderr).toEqual("");
   });
