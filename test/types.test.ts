e1a42b844cf14179c88c63a4ee9de621ee6bca09
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-08-26 11:12:29 +0900

    chore: fix or suppress ESLint issues

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,10 +1,12 @@
-import * as ybiq from ".."; // eslint-disable-line import/extensions
+import * as ybiq from "../lib/index.js"; // eslint-disable-line n/no-missing-import
 
 const noop = (arg?: unknown): void => {
   process.stdout.write(String(arg));
 };
 
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 ybiq.init().then(() => noop());
 ybiq.init({ cwd: "lib" }).then(() => noop());
 ybiq.init({ logger: (msg: string) => noop(msg) }).then(() => noop());
 ybiq.init({ cwd: "lib", logger: (msg: string) => noop(msg) }).then(() => noop);
+/* eslint-enable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
