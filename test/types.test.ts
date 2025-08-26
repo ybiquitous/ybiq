f93d29853ed5fc23f7705c8359203dd72a9372bc
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-08-26 11:16:39 +0900

    test: fix type errors

--- test/types.test.ts
+++ test/types.test.ts
@@ -4,9 +4,7 @@ const noop = (arg?: unknown): void => {
   process.stdout.write(String(arg));
 };
 
-/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 ybiq.init().then(() => noop());
 ybiq.init({ cwd: "lib" }).then(() => noop());
 ybiq.init({ logger: (msg: string) => noop(msg) }).then(() => noop());
 ybiq.init({ cwd: "lib", logger: (msg: string) => noop(msg) }).then(() => noop);
-/* eslint-enable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
