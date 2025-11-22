8b1f0c7ef7b7734114f4b4b8ac3cbf077339053a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-11-22 14:57:16 +0900

    feat(run)!: add `ybiq run` command (#1993)
    BREAKING CHANGE: This removed the [`npm-run-all`](https://github.com/mysticatea/npm-run-all) dependency because it has been unmaintained for a long time.

--- test/helpers/pkg.js
+++ test/helpers/pkg.js
@@ -1,3 +1,4 @@
 import { readFileSync } from "node:fs";
 
+/** @type {{ bin: string; } } */
 export const pkg = JSON.parse(readFileSync(new URL("../../package.json", import.meta.url)));
--- types/ybiq.d.ts
+++ types/ybiq.d.ts
@@ -1,8 +1,31 @@
 export type Logger = (msg: string) => void;
 export type CommandParams = {
-  cwd?: string | undefined;
-  logger?: Logger | undefined;
+  readonly cwd?: string | undefined;
+  readonly logger?: Logger | undefined;
 };
 
 export declare function init(params?: CommandParams): Promise<void>;
 type InitCommand = typeof init;
+
+export type RunLabeler = (script: string) => string;
+
+export type RunParams = {
+  readonly scripts: ReadonlyArray<string>;
+  readonly npm?: boolean | undefined;
+  readonly labeler?: RunLabeler | undefined;
+  readonly stdout?: NodeJS.WriteStream | undefined;
+  readonly stderr?: NodeJS.WriteStream | undefined;
+};
+
+export type RunResult = {
+  readonly script: string;
+  readonly success: boolean;
+  readonly code: number | undefined;
+  readonly error: Error | undefined;
+};
+
+export declare function run(params: Readonly<RunParams>): Promise<{
+  success: boolean;
+  results: Array<RunResult>;
+}>;
+type RunCommand = typeof run;
