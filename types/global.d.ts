e761049f766cd44a97e0d42657b516fe70f55d4d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-11-22 17:26:56 +0900

    fix(types): remove unneeded global types (#2011)

--- types/global.d.ts
+++
@@ -1,4 +0,0 @@
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34960#issuecomment-576906058
-declare let URL: typeof globalThis extends { URL: infer URLCtor }
-  ? URLCtor
-  : typeof import("url").URL; // eslint-disable-line @typescript-eslint/consistent-type-imports
