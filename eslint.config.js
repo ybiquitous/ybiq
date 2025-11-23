d790022bc43548565cccb5dd28490581c1bb6e94
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-11-23 10:32:18 +0900

    feat: migrate to ESLint flat config and update ESLint options in `package.json`

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js"; // eslint-disable-line n/no-extraneous-import
+import { defineConfig } from "eslint/config";
+
+import nodePlugin from "eslint-plugin-n";
+import tsPlugin from "typescript-eslint";
+import jestPlugin from "eslint-plugin-jest";
+
+export default defineConfig([
+  // Base
+  {
+    ignores: ["coverage", "dist", "tmp"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+      reportUnusedInlineConfigs: "error",
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    extends: [js.configs.recommended, nodePlugin.configs["flat/recommended-module"]],
+  },
+
+  // TypeScript
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./test/tsconfig.test.json"],
+      },
+    },
+    extends: [tsPlugin.configs.eslintRecommended, tsPlugin.configs.strictTypeChecked],
+  },
+
+  // Jest
+  {
+    files: ["**/*.test.js"],
+    extends: [jestPlugin.configs["flat/recommended"]],
+  },
+]);
