990375d03fc36855ec62a6e44a58e4a506595227
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-07-18 15:11:16 +0900

    feat: simplify type definitions
    - Inline types that are not directly used by users.
    - Refactor to inline a tiny module `lib/logger.js`.
    - Disable `skipLibCheck` for `tsc` to detect undefined type definitions.

--- types/ybiq.d.ts
+++ types/ybiq.d.ts
@@ -1,31 +1,20 @@
-export type Logger = (msg: string) => void;
-export type CommandParams = {
-  readonly cwd?: string | undefined;
-  readonly logger?: Logger | undefined;
-};
+export declare function init(params?: {
+  cwd?: string | undefined;
+  logger?: ((msg: string) => void) | undefined;
+}): Promise<void>;
 
-export declare function init(params?: CommandParams): Promise<void>;
-type InitCommand = typeof init;
-
-export type RunLabeler = (script: string) => string;
-
-export type RunParams = {
-  readonly scripts: ReadonlyArray<string>;
-  readonly npm?: boolean | undefined;
-  readonly labeler?: RunLabeler | undefined;
-  readonly stdout?: NodeJS.WriteStream | undefined;
-  readonly stderr?: NodeJS.WriteStream | undefined;
-};
-
-export type RunResult = {
-  readonly script: string;
-  readonly success: boolean;
-  readonly code: number | undefined;
-  readonly error: Error | undefined;
-};
-
-export declare function run(params: Readonly<RunParams>): Promise<{
+export declare function run(params: {
+  scripts: Array<string>;
+  npm?: boolean | undefined;
+  labeler?: ((script: string) => string) | undefined;
+  stdout?: NodeJS.WriteStream | undefined;
+  stderr?: NodeJS.WriteStream | undefined;
+}): Promise<{
   success: boolean;
-  results: Array<RunResult>;
+  results: Array<{
+    script: string;
+    success: boolean;
+    code: number | undefined;
+    error: Error | undefined;
+  }>;
 }>;
-type RunCommand = typeof run;
